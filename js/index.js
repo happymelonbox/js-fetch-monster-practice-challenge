b4541a905869e1d36d9fb20dee13fbe1839dee63
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-21 21:50:20 +1000

    fetch request working. Need to add syntax to display to page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
+.then(resp => resp.json())
+.then((data)=>{
+    console.log(data)
+})
 const createMonsterDiv = document.getElementById('create-monster')
 const monsterContainer = document.getElementById('monster-container')
 const backButton = document.getElementById('back')
