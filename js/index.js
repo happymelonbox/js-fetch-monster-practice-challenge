320aab61e507eb278fd052d80a0f81d1b6efd596
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-21 21:46:26 +1000

    Started working on the front end. Added the css file. Added the form and the input values

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const createMonsterDiv = document.getElementById('create-monster')
+const monsterContainer = document.getElementById('monster-container')
+const backButton = document.getElementById('back')
+const forwardButton = document.getElementById('forward')
+
+
+})
