5dec8d27b8d474b86971060fa216c05f9ce69df6
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-22 11:41:38 +1000

    Page forward and back working as they should

--- js/index.js
+++ js/index.js
@@ -1,21 +1,53 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
-.then(resp => resp.json())
-.then((data)=>{
-    const monsters = Object.values(data)
-    const createMonsterDiv = document.getElementById('create-monster')
-    let monsterContainerUl = document.getElementById('monster-container-ul')
+    const monsterContainerUl = document.getElementById('monster-container-ul')
     const backButton = document.getElementById('back')
     const forwardButton = document.getElementById('forward')
-    console.log(monsterContainerUl.parentNode.childNodes[1])
+    const URL = 'http://localhost:3000/monsters/?_limit=50&_page='
+    const UrlAllMonsters = 'http://localhost:3000/monsters/'
+
+    let pageNumber = 1
+    let totalMonsters, totalPages
+    let displayedPage = document.getElementById('pageNumber')
+    console.log(totalPages)
+
+    howManyPages()
+    fetchData(pageNumber)
+    displayedPage.innerHTML = pageNumber
+
+    backButton.addEventListener('click', function(){
+        if(pageNumber > 0){
+        pageNumber = pageNumber - 1
+        fetchData(pageNumber)
+        displayedPage.innerHTML = pageNumber
+        }
+    })
+
+    forwardButton.addEventListener('click', function(){
+        if(pageNumber <= totalPages){
+        pageNumber = (pageNumber + 1)
+        fetchData(pageNumber)
+        displayedPage.innerHTML = pageNumber
+    }})
+
+    function fetchData(page){
+    fetch(`${URL}${page}`)
+    .then(resp => resp.json())
+    .then(data=> displayMonster(data))
+    }
+    function displayMonster(obj){
+    const monsters = Object.values(obj)
     let monster, mCListItem, monsterName, monsterDetails, monsterAge, monsterDescription
     let mCList = monsterContainerUl.appendChild(document.createElement('li'))
+    let currentlyDisplayed = document.querySelectorAll('li.displayedMonsters')
+    for (i=0;i<currentlyDisplayed.length;i++){
+        currentlyDisplayed[i].remove()
+    }
     for(let i=0; i<monsters.length; i++){
        monster = monsters[i]
        monsterName = monster.name
-       monsterAge = monster.age
+       monsterAge = parseInt(monster.age)
        monsterDescription = monster.description
+       mCList.setAttribute('class', 'displayedMonsters')
        mCListItem = mCList.appendChild(document.createElement('li'))
        mCListItem.innerHTML = monsterName
        mCListItem.setAttribute('id', monsterName)
@@ -23,15 +55,18 @@ fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
        monsterDetails.setAttribute('class', 'monster-info')
        monsterAgeLi = monsterDetails.appendChild(document.createElement('li'))
        monsterAgeLi.setAttribute('class', 'monster-age-li')
-       monsterAgeLi.innerHTML = "<strong>Age: </strong>"+monsterAge
+       monsterAgeLi.innerHTML = "<strong>Age: </strong>"+monsterAge+' years old'
        monsterDescLi = monsterDetails.appendChild(document.createElement('li'))
        monsterDescLi.setAttribute('class', 'monster-description-li')
        monsterDescLi.innerHTML = "<strong>Description: </strong>"+monsterDescription
     }
-    console.log(data)
-    console.log(monster)
-})
-
-
-
+    }
+    function howManyPages(){
+        fetch(UrlAllMonsters)
+        .then(resp => resp.json())
+        .then((json)=>{
+            totalMonsters = json.length
+            totalPages = totalMonsters/50
+        })
+    }
 })
