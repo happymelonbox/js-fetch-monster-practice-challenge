b50556fb3511dab27d1263300ec8c0105537cf23
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-23 22:07:57 +1000

    Project Complete - ;Put aside the functionality to check if the new monster aleady exists for now, will revisit it when I have a bit more knowledge. For now, the application works as requested in the documentation

--- js/index.js
+++ js/index.js
@@ -7,113 +7,76 @@ document.addEventListener('DOMContentLoaded', function(){
     const UrlAllMonsters = 'http://localhost:3000/monsters/'
 
     let pageNumber = 1
-    let totalMonsters
-    let totalPages
-    let createMonsterName
-    let createMonsterAge
-    let createMonsterDescription
     let displayedPage = document.getElementById('pageNumber')
+    let totalMonsters, totalPages, createMonsterName, createMonsterAge, createMonsterDescription
 
     howManyPages()
     fetchData(pageNumber)
     displayedPage.innerHTML = pageNumber
 
+    backButton.addEventListener('click', function(){pageBack()})
+    forwardButton.addEventListener('click', function(){pageForward()})
+    monsterForm.addEventListener('submit', function(event){submitMonster(event)})
 
-    backButton.addEventListener('click', function pageBack(){
-        if(pageNumber > 1){
-        pageNumber = pageNumber - 1
-        fetchData(pageNumber)
-        displayedPage.innerHTML = pageNumber
-        }
-    })
-    forwardButton.addEventListener('click', function pageForward(){
-        if(pageNumber <= totalPages){
-        pageNumber = (pageNumber + 1)
-        fetchData(pageNumber)
-        displayedPage.innerHTML = pageNumber
-        }
-    })
-
-    function fetchData(page){
-    fetch(`${URL}${page}`)
-    .then(resp => resp.json())
-    .then(data=> displayMonster(data))
-    }
-
-    monsterForm.addEventListener('submit', function submitMonster(event){
+    function submitMonster(event){
         event.preventDefault()
         createMonsterName = document.getElementById('inputName').value
         createMonsterAge = document.getElementById('inputAge').value
         createMonsterDescription = document.getElementById('inputDescription').value
-        fetch(UrlAllMonsters)
-        .then(resp=>resp.json())
-        .then((newData) => {
-            let monsterObjValues = Object.values(newData)
-            let monsterList, monsterListAge, monsterListName, monsterListDesc
-            let itExists = false
-            for(value in monsterObjValues){
-                monsterList = monsterObjValues[value]
-                monsterListName = monsterList.name,
-                monsterListAge = monsterList.age,
-                monsterListDesc = monsterList.description
-            
-            if  (createMonsterName == monsterListName &&
-                createMonsterAge == monsterListAge &&
-                createMonsterDescription == monsterListDesc){
-
-                return itExists = true
-                }}
-                console.log(itExists)
-            
-            // if(itExists){
-            //     alert(`Monster ${createMonsterName} already exists`
-            // )} 
-            // else {
-            //     console.log(createMonsterName + createMonsterAge + createMonsterDescription)
-            //     fetch(UrlAllMonsters, {
-            //         method: 'POST',
-            //         headers: {
-            //             'Content-Type': 'application/json',
-            //             Accept: 'application/json'
-            //         },
-            //         body: JSON.stringify({
-            //             name: `${createMonsterName}`,
-            //             age: `${createMonsterAge}`,
-            //             description: `${createMonsterDescription}`
-            //         })
-            //     })
-            //     .then(resp=>resp.json())
-            //     .then(data=>console.log(data))
-            // }
+        fetch(UrlAllMonsters, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                name: `${createMonsterName}`,
+                age: `${createMonsterAge}`,
+                description: `${createMonsterDescription}`
+            })
         })
-    })
+        .then(resp=>resp.json())
+        .then((data)=>{
+            console.log(data)
+            created = document.getElementById('created')
+            created.innerHTML = `New monster '${createMonsterName}' has been added!`
+        })
+    }
+
     function displayMonster(obj){
-    const monsters = Object.values(obj)
-    let monster, mCListItem, monsterName, monsterDetails, monsterAge, monsterDescription
-    let mCList = monsterContainerUl.appendChild(document.createElement('li'))
-    let currentlyDisplayed = document.querySelectorAll('li.displayedMonsters')
-    for (i=0;i<currentlyDisplayed.length;i++){
-        currentlyDisplayed[i].remove()
+        const monsters = Object.values(obj)
+        let monster, mCListItem, monsterName, monsterDetails, monsterAge, monsterDescription
+        let mCList = monsterContainerUl.appendChild(document.createElement('li'))
+        let currentlyDisplayed = document.querySelectorAll('li.displayedMonsters')
+        for (i=0;i<currentlyDisplayed.length;i++){
+            currentlyDisplayed[i].remove()
+        }
+        for(let i=0; i<monsters.length; i++){
+            monster = monsters[i]
+            monsterName = monster.name
+            monsterAge = parseInt(monster.age)
+            monsterDescription = monster.description
+            mCList.setAttribute('class', 'displayedMonsters')
+            mCListItem = mCList.appendChild(document.createElement('li'))
+            mCListItem.innerHTML = monsterName
+            mCListItem.setAttribute('id', monsterName)
+            monsterDetails = mCListItem.appendChild(document.createElement('ul'))
+            monsterDetails.setAttribute('class', 'monster-info')
+            monsterAgeLi = monsterDetails.appendChild(document.createElement('li'))
+            monsterAgeLi.setAttribute('class', 'monster-age-li')
+            monsterAgeLi.innerHTML = "<strong>Age: </strong>"+monsterAge+' years old'
+            monsterDescLi = monsterDetails.appendChild(document.createElement('li'))
+            monsterDescLi.setAttribute('class', 'monster-description-li')
+            monsterDescLi.innerHTML = "<strong>Description: </strong>"+monsterDescription
+        }
     }
-    for(let i=0; i<monsters.length; i++){
-       monster = monsters[i]
-       monsterName = monster.name
-       monsterAge = parseInt(monster.age)
-       monsterDescription = monster.description
-       mCList.setAttribute('class', 'displayedMonsters')
-       mCListItem = mCList.appendChild(document.createElement('li'))
-       mCListItem.innerHTML = monsterName
-       mCListItem.setAttribute('id', monsterName)
-       monsterDetails = mCListItem.appendChild(document.createElement('ul'))
-       monsterDetails.setAttribute('class', 'monster-info')
-       monsterAgeLi = monsterDetails.appendChild(document.createElement('li'))
-       monsterAgeLi.setAttribute('class', 'monster-age-li')
-       monsterAgeLi.innerHTML = "<strong>Age: </strong>"+monsterAge+' years old'
-       monsterDescLi = monsterDetails.appendChild(document.createElement('li'))
-       monsterDescLi.setAttribute('class', 'monster-description-li')
-       monsterDescLi.innerHTML = "<strong>Description: </strong>"+monsterDescription
-    }
-    }
+
+    function fetchData(page){
+        fetch(`${URL}${page}`)
+        .then(resp => resp.json())
+        .then(data=> displayMonster(data))
+        }
+
     function howManyPages(){
         fetch(UrlAllMonsters)
         .then(resp => resp.json())
@@ -121,5 +84,21 @@ document.addEventListener('DOMContentLoaded', function(){
             totalMonsters = json.length
             totalPages = totalMonsters/50
         })
+    }
+
+    function pageBack(){
+        if(pageNumber > 1){
+        pageNumber = pageNumber - 1
+        fetchData(pageNumber)
+        displayedPage.innerHTML = pageNumber
+        }
+    }
+
+    function pageForward(){
+        if(pageNumber <= totalPages){
+        pageNumber = (pageNumber + 1)
+        fetchData(pageNumber)
+        displayedPage.innerHTML = pageNumber
+        }
     }
 })
